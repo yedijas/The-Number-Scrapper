aa1254203feacb0d2b470fc3173cd8db8a629495
Aditya YM Situmeang <bananab9001@gmail.com>
2015-01-26 19:58:05 +0700

    first push

---
+++ NumbersScrapper/Helper/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPUScrapper.Helper
+{
+    class Helper
+    {
+        public static string ValidateDir(string dir)
+        {
+            if (!dir.Last().Equals('\\'))
+                return dir + '\\';
+            else
+                return dir;
+        }
+    }
+}
